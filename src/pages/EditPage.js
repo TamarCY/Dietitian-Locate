dcd3692c927d8c1548c200395f0b62235f99362c
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-01-24 11:49:39 +0200

    tampale edit page

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,91 +1,93 @@
-// import React from 'react'
-// import Form from 'react-bootstrap/Form'
-// import Button from 'react-bootstrap/Button'
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Col } from 'react-bootstrap';
 
-// class EditPage extends React.Component {
-//     render(){
-//         return(
-//             <div>
+class EditPage extends React.Component {
+    render(){
+        return(
+            <div>
 
-// <Form>
-//   <Form.Row>
-//     <Form.Group as={Col} controlId="formGridEmail">
-//       <Form.Label>Email</Form.Label>
-//       <Form.Control type="email" placeholder="Enter email" />
-//     </Form.Group>
+<Form>
+    
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
 
-//     <Form.Group as={Col} controlId="formGridPassword">
-//       <Form.Label>Password</Form.Label>
-//       <Form.Control type="password" placeholder="Password" />
-//     </Form.Group>
-//   </Form.Row>
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
 
-//   <Form.Group controlId="formGridAddress1">
-//     <Form.Label>Address</Form.Label>
-//     <Form.Control placeholder="1234 Main St" />
-//   </Form.Group>
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
 
-//   <Form.Group controlId="formGridAddress2">
-//     <Form.Label>Address 2</Form.Label>
-//     <Form.Control placeholder="Apartment, studio, or floor" />
-//   </Form.Group>
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
 
-//   <Form.Row>
-//     <Form.Group as={Col} controlId="formGridCity">
-//       <Form.Label>City</Form.Label>
-//       <Form.Control />
-//     </Form.Group>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
 
-//     <Form.Group as={Col} controlId="formGridState">
-//       <Form.Label>State</Form.Label>
-//       <Form.Control as="select" defaultValue="Choose...">
-//         <option>Choose...</option>
-//         <option>...</option>
-//       </Form.Control>
-//     </Form.Group>
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
 
-//     <Form.Group as={Col} controlId="formGridZip">
-//       <Form.Label>Zip</Form.Label>
-//       <Form.Control />
-//     </Form.Group>
-//   </Form.Row>
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
 
-//   <Form.Group id="formGridCheckbox">
-//     <Form.Check type="checkbox" label="Check me out" />
-//   </Form.Group>
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
 
-//   <Button variant="primary" type="submit">
-//     Submit
-//   </Button>
-// </Form>
-// </div>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
 
-//             // <div className="edit-page">
-//             //     <Form>
-//             //     <Form.Group controlId="formBasicEmail">
-//             //         <Form.Label>Email address</Form.Label>
-//             //         <Form.Control type="email" placeholder="Enter email" />
-//             //         <Form.Text className="text-muted">
-//             //         We'll never share your email with anyone else.
-//             //         </Form.Text>
-//             //     </Form.Group>
+            // <div className="edit-page">
+            //     <Form>
+            //     <Form.Group controlId="formBasicEmail">
+            //         <Form.Label>Email address</Form.Label>
+            //         <Form.Control type="email" placeholder="Enter email" />
+            //         <Form.Text className="text-muted">
+            //         We'll never share your email with anyone else.
+            //         </Form.Text>
+            //     </Form.Group>
 
-//             //     <Form.Group controlId="formBasicPassword">
-//             //         <Form.Label>Password</Form.Label>
-//             //         <Form.Control type="password" placeholder="Password" />
-//             //     </Form.Group>
-//             //     <Form.Group controlId="formBasicCheckbox">
-//             //         <Form.Check type="checkbox" label="Check me out" />
-//             //     </Form.Group>
-//             //     <Button variant="primary" type="submit">
-//             //         Submit
-//             //     </Button>
-//             //     </Form>
-//             //     <h2>I am  EditPage</h2>
-//             // </div>
-//         )
-//     }
+            //     <Form.Group controlId="formBasicPassword">
+            //         <Form.Label>Password</Form.Label>
+            //         <Form.Control type="password" placeholder="Password" />
+            //     </Form.Group>
+            //     <Form.Group controlId="formBasicCheckbox">
+            //         <Form.Check type="checkbox" label="Check me out" />
+            //     </Form.Group>
+            //     <Button variant="primary" type="submit">
+            //         Submit
+            //     </Button>
+            //     </Form>
+            //     <h2>I am  EditPage</h2>
+            // </div>
+        )
+    }
         
-// }
+}
 
-// export default EditPage;
+export default EditPage;
