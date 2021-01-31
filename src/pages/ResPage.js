e090f3bc64ffcac109eead8eea5b7e289f38eb66
Tamar Cohen Yogev <cohen.tamar@gmail.com>
2021-01-31 17:18:07 +0200

    before font awsom

--- src/pages/ResPage.js
+++ src/pages/ResPage.js
@@ -22,12 +22,13 @@ class ResPage extends React.Component {
         console.log(this.props.results);
         
         for (let i=0; i<this.props.results.length; i++){
-            const rowContent = <tr>
-                <td><img className="img-fluid" src={this.props.results[i].imgUrl}/></td>
+            const rowContent = <tr >
+                <td><img className="img-fluid w-50" src={this.props.results[i].imgUrl}/></td>
                 <td>{this.props.results[i].fname}</td>
                 <td>{this.props.results[i].lname}</td>
                 <td>{this.props.results[i].phone}</td>
-                <td>{this.props.results[i].email}</td>
+                <td><a></a>{this.props.results[i].email}</td>
+
 
                 {/* <td>{this.props.results[i].clinics}</td> */}          
             </tr>
